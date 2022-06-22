b03643987799f4e7069f41e618953047398e0537
jason decker <jdeck@MacBook-Pro.local>
2022-06-21 20:04:16 -0400

    Completed MVP tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Link, Route, Switch } from "react-router-dom";
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList'
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -12,6 +14,7 @@ export default function App () {
       axios
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -30,7 +33,14 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route path={"/movies/:id"}>
+          <Movie movies={movieList}/>
+        </Route>
+        <Route path="/">
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 }
